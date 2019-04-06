1446a0c837c6ac2a27b1f4e33db5051155fb78d3
orris27 <gsorris27@gmail.com>
2019-04-06 20:35:30 +0800

    add lstm

--- utils.py
+++ utils.py
@@ -4,3 +4,6 @@ def softmax(x):
     e_x = np.exp(x - np.max(x)) # Same result as np.exp(x) while avoiding overflow
     return e_x / e_x.sum(axis=0)
 
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
